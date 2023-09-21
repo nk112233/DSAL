715f95eb385bfeb3a5bbd42cadbc7a024984c90d
Nikunj Kadu <134802492+nk112233@users.noreply.github.com>
2023-09-21 16:01:01 +0530

    Update infx.cpp

--- infx.cpp
+++ infx.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include </home/pict/SE_23312/DSAL/stack.cpp>
 using namespace std;
-int main(){
+
+int isp(Stack s){
+	x = s.peep();
+	if(x == '^'){
+		return 3;
+	}
+	else if(x == '*' || x == '/'){
+		return 2;
+
+	}
+	else if(x == '+' || x == '-'){
+		return 1;
 	
+	}
+
+
+
+}
+int icp(char a){
+	if(a == '^'){
+		return 3;
+	}
+	else if(a == '*' || a == '/'){
+		return 2;
+
+	}
+	else if(a == '+' || a == '-'){
+		return 1;
+	
+	}
+
+}
+char [] infxtpstfx(){
+	
+
+
+
+
+
+}
+char postfix[20];
+int main(){
 
 
 

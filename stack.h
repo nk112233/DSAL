fe0fd5ac0831505d9c493bbf8a24a777a1725f83
nk112233 <nikunjkadu19@gmail.com>
2023-09-24 19:54:32 +0530

    Added stack.h

---
+++ stack.h
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Stack{
+    private: struct Node {
+    T elem;
+    Node *next;
+    
+    
+};
+    private : Node *tos;
+    public: Stack(){
+    	tos = NULL;
+    }
+public:
+    bool push(T elem){
+       
+        Node *temp = new Node;
+        if(temp == NULL)
+            return 0;
+        temp->elem = elem;
+        temp->next = tos;
+        tos = temp;
+        return 1;
+
+    }
+public:
+    T pop(){
+        Node *temp = new Node;
+        temp = tos;
+        tos = temp->next;
+        return temp->elem;
+
+
+
+    }
+public:
+    bool IsEmpty(){
+        if(tos == NULL){
+            return 1;
+
+        }
+
+
+    }
+public:
+    T peep(){
+        if(tos != NULL){
+            return tos->elem;
+        }
+            
+        else{
+            return 0;
+        }
+
+
+    }
+
+
+
+};
+/*int main(){
+	Stack s;
+	cout << s.peep()<<endl;
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+*/

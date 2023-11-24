351c09cb68d6ced298e0178457655cbb27058df6
nk112233 <nikunjkadu19@gmail.com>
2023-11-24 20:31:51 +0530

    Modified heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,94 +1,54 @@
 #include <iostream>
 using namespace std;
-class MyHeap{
-    public: void heapify(int arr[], int N, int i){
- 
-    // Initialize largest as root
-    int largest = i;
- 
-    // left = 2*i + 1
-    int l = 2 * i + 1;
- 
-    // right = 2*i + 2
-    int r = 2 * i + 2;
- 
-    // If left child is larger than root
-    if (l < N && arr[l] > arr[largest])
-        largest = l;
- 
-    // If right child is larger than largest
-    // so far
-    if (r < N && arr[r] > arr[largest])
-        largest = r;
- 
-    // If largest is not root
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
- 
-        // Recursively heapify the affected
-        // sub-tree
-        heapify(arr, N, largest);
+
+class MyHeap {
+public:
+    void heapify(int arr[], int N, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+            heapify(arr, N, largest);
         }
     }
-    public: void heapSort(int arr[], int N){
- 
-    // Build heap (rearrange array)
-    for (int i = N / 2 - 1; i >= 0; i--)
-        heapify(arr, N, i);
- 
-    // One by one extract an element
-    // from heap
-    for (int i = N - 1; i > 0; i--) {
- 
-        // Move current root to end
-        swap(arr[0], arr[i]);
- 
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
+
+    void heapSort(int arr[], int N) {
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+        int  j = 1;
+        for (int i = N - 1; i > 0; i--) {
+            swap(arr[0], arr[i]);
+            heapify(arr, i, 0);
+            cout<< "iteration " << j << ": ";
+            printArray(arr, N);
+        }
     }
-}
-void printArray(int arr[], int N){
-    for (int i = 0; i < N; ++i)
-        cout << arr[i] << " ";
-    cout << "\n";
-} 
 
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
-    int N = sizeof(arr) / sizeof(arr[0]);
- 
-    // Function call
-    heapSort(arr, N);
- 
-    cout << "Sorted array is \n";
-    printArray(arr, N);
-}
-
-
-
-
-
-
-
+    void printArray(int arr[], int N) {
+        for (int i = 0; i < N; ++i)
+            cout << arr[i] << " ";
+        cout << "\n";
+    }
 };
-int main(){
+
+int main() {
     MyHeap heap;
-    int arr[] = {7,6,2,8,5,4};
-    heap.heapify(arr,6,7);
-
-
-
-
-
-
-
-
-
-
-
-
-
+    int arr[] = { 7, 6, 2, 8, 5, 4 };
+    int N = sizeof(arr) / sizeof(arr[0]);
+    cout << "Original array: ";
+    heap.printArray(arr, N);
+    heap.heapSort(arr, N);
+ 
+    cout << "Sorted array is "<<endl;
+    heap.printArray(arr, N);
 
     return 0;
 }
